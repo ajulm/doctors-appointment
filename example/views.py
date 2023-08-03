8d249b64f581aa064da81041b48b3cf881a30023
ajulMaharjan <ajul@codepros.com.np>
2023-08-03 22:45:15 +0545

    updated site domain

--- example/views.py
+++ example/views.py
@@ -8,7 +8,8 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Hello from Ajul Maharjan!</h1>
+            <p>Site is currently on development.</p>
             <p>The current time is { now }.</p>
         </body>
     </html>
